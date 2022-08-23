5dcd892f89a45d0a6a4f4a13e5355902a0dd3769
Yevhen Lebid <ealebed@gmail.com>
2022-08-23 14:05:41 +0300

    Update version.go

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -22,12 +22,12 @@ import (
 )
 
 // Version represents main version number being run right now.
-var Version = "0.0.1"
+var Version = "1.0.0"
 
 // ReleasePhase represents pre-release marker for the version. If this is an empty string,
 // then the release is a final release. Otherwise this is a pre-release
 // version e.g. "dev", "alpha", etc.
-var ReleasePhase = "dev"
+var ReleasePhase = ""
 
 // String prints the version of the jarvis CLI.
 func String() string {
